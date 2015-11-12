2d9f9935f4111d0947d61b4115e021d00b3a2554
Matthew Brett <matthew.brett@gmail.com>
2015-11-12 11:49:58 -0800

    WIP: slice time script

---
+++ day3/slice_time_image.py
@@ -0,0 +1,15 @@
+""" Slice time image
+
+This is a Python script.
+
+You can run this script from the command line with::
+
+    python slice_time_script.py an_image.nii
+
+or from the IPython console with::
+
+    run slice_time_script.py an_image.nii
+
+You can check the individual functions with ``import`` in the IPython console::
+
+    a
